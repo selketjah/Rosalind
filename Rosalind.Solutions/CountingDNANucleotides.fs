67094279994d482157194755cdd210d0842230ce
selketjah <gien.verschatse@gmail.com>
2016-01-11 20:09:05 +0100

    implementing counting dna nucleotides

--- Rosalind.Solutions/CountingDNANucleotides.fs
+++ Rosalind.Solutions/CountingDNANucleotides.fs
@@ -2,4 +2,29 @@
 
 module CountingDNANucleotides =
 
-  let sampleData = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+  let dnaString = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+  let countAdinene =
+    dnaString
+    |> Seq.countBy (fun s -> if s = 'A' then "A" else "")
+    |> Seq.toList
+
+  let countCytosine =
+    dnaString
+    |> Seq.countBy (fun s -> if s = 'C' then "C" else "")
+    |> Seq.toList
+
+  let countGuanine =
+    dnaString
+    |> Seq.countBy (fun s -> if s = 'G' then "G" else "")
+    |> Seq.toList
+
+  let countThymine =
+      dnaString
+      |> Seq.countBy (fun s -> if s = 'T' then "T" else "")
+      |> Seq.toList
+
+  let countMolecules =
+    countAdinene @ countCytosine @ countGuanine @ countThymine
+    |> List.sort
+    |> Seq.skipWhile (fun (molecule, count) -> molecule = "" )
