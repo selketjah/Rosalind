c31f00aad4fbf701cf7146d558b3d060cd5ff886
selketjah <gien.verschatse@gmail.com>
2016-01-17 13:52:06 +0100

    first cleanup of counting dna nucleotides

--- Rosalind.Solutions/CountingDNANucleotides.fs
+++ Rosalind.Solutions/CountingDNANucleotides.fs
@@ -5,36 +5,24 @@ open System.IO
 
 module CountingDNANucleotides =
 
-  //let dnaChain = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+  let dnaChain = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 
   let data =
     "Rosalind.Solutions/input/rosalind_dna.txt"
-    |> File.ReadAllLines
+    |> File.ReadAllText
 
-  let dnaChain = List.ofSeq data |> List.head
+  let countLetter sequence letter =
+    sequence
+    |> Seq.filter (fun s -> s = letter)
+    |> Seq.length
 
-  let adinene =
-    dnaChain
-    |> Seq.countBy (fun s -> if s = 'A' then "A" else "")
-    |> Seq.toList
+  let adenine = countLetter dnaChain 'A'
 
-  let cytosine =
-    dnaChain
-    |> Seq.countBy (fun s -> if s = 'C' then "C" else "")
-    |> Seq.toList
+  let cytosine = countLetter dnaChain 'C'
 
-  let guanine =
-    dnaChain
-    |> Seq.countBy (fun s -> if s = 'G' then "G" else "")
-    |> Seq.toList
+  let guanine = countLetter dnaChain 'G'
 
-  let thymine =
-      dnaChain
-      |> Seq.countBy (fun s -> if s = 'T' then "T" else "")
-      |> Seq.toList
+  let thymine = countLetter dnaChain 'T'
 
   let countMolecules =
-    adinene @ cytosine @ guanine @ thymine
-    |> List.sort
-    |> Seq.skipWhile (fun (molecule, count) -> molecule = "" )
-    |> Seq.iter (fun (a, b) -> printf "%d " b)
+    printf "%d %d %d %d" adenine cytosine guanine thymine
